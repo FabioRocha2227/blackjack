343e5062b60dad2c40a5ea815fd2c744cf543496
FabioRocha2227 <92931270+FabioRocha2227@users.noreply.github.com>
2026-07-15 13:07:20 +0100

    Fixed some lag issues

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,24 @@
 import { CARD_BACK_SRC, cardImageSrc } from "../utils/cardImages.js";
 
+const CARD_IMAGE_SIZE = 64;
+
 export default function Card({ card, hidden = false, flip = false }) {
   return (
     <div className={`card ${hidden ? "back" : ""} ${flip ? "flip" : ""}`}>
       {hidden ? (
-        <img src={CARD_BACK_SRC} alt="Hidden card" />
+        <img
+          src={CARD_BACK_SRC}
+          alt="Hidden card"
+          width={CARD_IMAGE_SIZE}
+          height={CARD_IMAGE_SIZE}
+        />
       ) : (
-        <img src={cardImageSrc(card)} alt={`${card.rank} of ${card.suit}`} />
+        <img
+          src={cardImageSrc(card)}
+          alt={`${card.rank} of ${card.suit}`}
+          width={CARD_IMAGE_SIZE}
+          height={CARD_IMAGE_SIZE}
+        />
       )}
     </div>
   );
